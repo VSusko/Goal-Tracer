cc2eed13ae3acfd8ba6de2403a14064c6e8d79ee
VSusko <suskovictor@gmail.com>
2026-06-05 08:06:27 -0300

    rotas definidas

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('metas/', views.metas, name='metas'),
+    path('relatorios/', views.relatorios, name='relatorios'),
+    path('hoje/', views.hoje, name='hoje'),
+    path('calendario/', views.calendario, name='calendario'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,15 @@ from .models import Mensagem
 def index(request):
     mensagens = Mensagem.objects.all()
     return render(request, "home/index.html", {"mensagens": mensagens})
+
+def metas(request):
+    return render(request, "home/metas.html")
+
+def relatorios(request):
+    return render(request, "home/relatorios.html")
+
+def hoje(request):
+    return render(request, "home/hoje.html")
+
+def calendario(request):
+    return render(request, "home/calendario.html")
