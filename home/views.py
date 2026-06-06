d82d75c22b1fcf10f8e95b4ad61bdcb2d5083bc9
VSusko <suskovictor@gmail.com>
2026-06-06 08:49:08 -0300

    primeiros passos javascript

--- home/views.py
+++ home/views.py
@@ -3,15 +3,27 @@ from .models import Atividade
 
 
 def index(request):
-    # atividades = Atividade.objects.all()
-    # dias_semana = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"]
+    dias = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"]
 
-    # return render(request,"home/index.html",{"atividades": atividades, "dias_semana": dias_semana})
-    dias = []
+    # Obtendo todas as atividades do banco de dados
+    atividades = Atividade.objects.all()
+    
+    # Convertendo a duração de minutos para horas e armazenando em um atributo temporário
+    for atividade in atividades:
+        atividade.duracao_horas = atividade.duracao_minutos / 60.0
 
-    for nome_dia in ["Segunda","Terça","Quarta","Quinta","Sexta","Sábado","Domingo"] : dias.append({"nome": nome_dia,
-        "atividades": Atividade.objects.filter(dia_semana=nome_dia)})
-    return render(request,"home/index.html",{"dias": dias})
+    # Criando uma lista para armazenar os dias da semana e suas atividades
+    atividades_por_dia = []
+    
+    # Para cada dia, verifica se existe uma atividade
+    for dia in dias:
+        # Filtra as atividades e coloca na lista apenas as que correspondem ao dia atual
+        lista = [a for a in atividades if a.dia_semana == dia]
+        
+        # Adiciona a lista gerada
+        atividades_por_dia.append((dia, lista))
+
+    return render(request,"home/index.html",{"dias": dias, "atividades_por_dia": atividades_por_dia})
 
 def metas(request):
     return render(request, "home/metas.html")
---
+++ static/home/script.js
@@ -0,0 +1,5 @@
+const input = document.getElementById("NovaAtividade");
+
+input.addEventListener("click", () => {
+    console.log("teste");
+});
