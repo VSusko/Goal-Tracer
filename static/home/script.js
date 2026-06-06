0648e7ed059e5463bc82e3bfe7736d655b71d859
VSusko <suskovictor@gmail.com>
2026-06-06 15:31:08 -0300

    caixa de nova atividade

--- static/home/script.js
+++ static/home/script.js
@@ -1,5 +1,15 @@
-const input = document.getElementById("NovaAtividade");
+const botaoNovaAtividade = document.getElementById("botao_nova_atividade");
+const caixa_nova_atividade = document.getElementById("caixa_nova_atividade");
 
-input.addEventListener("click", () => {
-    console.log("teste");
+// Botao nova atividade
+botaoNovaAtividade.addEventListener("click", () => {
+    caixa_nova_atividade.classList.remove("hidden");
 });
+
+// Botao cancelar nova atividade
+const botaoCancelar = document.getElementById("botao_adicionar_cancelar");
+
+botaoCancelar.addEventListener("click", () => {
+    caixa_nova_atividade.classList.add("hidden");
+});
+
